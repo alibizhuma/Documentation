10095ebb582be6661540c4fa58d6c26972bcfcfe
alibizhuma <156778479+alibizhuma@users.noreply.github.com>
2024-04-21 05:59:38 +0500

    Delete html directory

--- html/search/classes_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['mainch_0',['MainCh',['../classmain_ch_1_1_main_ch.html',1,'mainCh']]]
-];
--- html/search/functions_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['update_0',['update',['../classbutton_1_1_button.html#a997b659d6b85a905cfef558f710b86bf',1,'button::Button']]]
-];
